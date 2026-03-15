18886551d8a7c76cc562e1d13bc4d65c39149d81
Henrique <henriquesdm7@gmail.com>
2026-03-14 21:52:26 -0300

    WIP feat: formulário de login

---
+++ app/pages/login.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type {AuthFormField} from "@nuxt/ui/components/AuthForm.vue";
+
+definePageMeta({
+  layout: "guest",
+})
+
+const toast = useToast();
+
+const fields: AuthFormField[] = [{
+  name: 'email',
+  type: 'email',
+  label: 'Email',
+  required: true,
+}, {
+  name: 'password',
+  type: 'password',
+  label: 'Senha',
+  required: true,
+}]
+
+const providers = [{
+  label: 'Github',
+  icon: 'i-simple-icons-github',
+  onClick: () => {
+    toast.add({title: 'Login com Github', description: 'Em breve', color: 'warning', icon: 'i-simple-icons-github'});
+  }
+}]
+</script>
+
+<template>
+  <div class="flex justify-center p-4">
+    <UPageCard class="w-full max-w-md">
+      <UAuthForm title="Acessar minha conta"
+                 description="Faça login para acessar sua conta"
+                 :fields="fields"
+                 :providers="providers"
+                 separator="ou"/>
+    </UPageCard>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
