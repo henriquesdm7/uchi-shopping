b35cce41abb2d9cb683df09fbf0835d39812fe0e
Henrique <henriquesdm7@gmail.com>
2026-03-15 03:03:00 -0300

    WIP feat: login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type {AuthFormField} from "@nuxt/ui/components/AuthForm.vue";
+import {type PasswordLoginInput, PasswordLoginSchema} from "#shared/utils/auth.schema";
+import type {FormSubmitEvent} from "@nuxt/ui/runtime/types";
 
 definePageMeta({
   layout: "guest",
@@ -26,6 +28,10 @@ const providers = [{
     toast.add({title: 'Login com Github', description: 'Em breve', color: 'warning', icon: 'i-simple-icons-github'});
   }
 }]
+
+async function onSubmit(event: FormSubmitEvent<PasswordLoginInput>) {
+  console.log(event.data);
+}
 </script>
 
 <template>
@@ -33,9 +39,11 @@ const providers = [{
     <UPageCard class="w-full max-w-md">
       <UAuthForm title="Acessar minha conta"
                  description="Faça login para acessar sua conta"
+                 :schema="PasswordLoginSchema"
                  :fields="fields"
                  :providers="providers"
-                 separator="ou"/>
+                 separator="ou"
+                 @submit="onSubmit"/>
     </UPageCard>
   </div>
 </template>
