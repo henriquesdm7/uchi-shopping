49c2388dc74d90b07cc7355a4e01e18238a75e0b
Henrique <henriquesdm7@gmail.com>
2026-03-20 00:33:30 -0300

    feat: develoment creation

--- server/middleware/redirect-guests.ts
+++ server/middleware/redirect-guests.ts
@@ -1,14 +1,14 @@
 // This middleware will redirect all guests to /login and logged in users to / if they try to access /login
 
 export default defineEventHandler(async (event) => {
-  const session = await getUserSession(event);
-  const route = getRequestURL(event).pathname;
+  // const session = await getUserSession(event);
+  // const route = getRequestURL(event).pathname;
 
-  const isLoggedIn = !!session && !!session.user;
+  // const isLoggedIn = !!session && !!session.user;
 
-  if (!isLoggedIn && !route.endsWith('login')) {
-    return sendRedirect(event, '/login')
-  } else if (isLoggedIn && route.endsWith('login')) {
-    return sendRedirect(event, '/')
-  }
+  // if (!isLoggedIn && !route.endsWith('login')) {
+  //   return sendRedirect(event, '/login')
+  // } else if (isLoggedIn && route.endsWith('login')) {
+  //   return sendRedirect(event, '/')
+  // }
 })
